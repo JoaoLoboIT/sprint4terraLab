bfaa7645d6f69d41e89b30fe9ab3046d6851ee8e
João Pedro Lobo <JoaoLoboIT@hotmail.com>
2025-07-11 11:09:57 -0300

    crud para os pontos

--- app.py
+++ app.py
@@ -77,3 +77,24 @@ def ver_meus_pontos(email):
 
     pontos_do_usuario = usuario.pontos
     return render_template('listar_pontos.html', usuario=usuario, lista_de_pontos=pontos_do_usuario)
+
+@app.route('/ponto/<int:ponto_id>/alterar', methods=['GET', 'POST'])
+def alterar_ponto(ponto_id):
+    ponto_para_alterar = Ponto.query.get_or_404(ponto_id)
+
+    if request.method == 'POST':
+        ponto_para_alterar.latitude = request.form['latitude']
+        ponto_para_alterar.longitude = request.form['longitude']
+        ponto_para_alterar.descricao = request.form['descricao']
+        db.session.commit()
+        return redirect(url_for('ver_meus_pontos', email=ponto_para_alterar.autor.email))
+
+    return render_template('alterar_ponto.html', ponto=ponto_para_alterar)
+
+@app.route('/ponto/<int:ponto_id>/remover', methods=['POST'])
+def remover_ponto(ponto_id):
+    ponto_para_remover = Ponto.query.get_or_404(ponto_id)
+    email_do_autor = ponto_para_remover.autor.email
+    db.session.delete(ponto_para_remover)
+    db.session.commit()
+    return redirect(url_for('ver_meus_pontos', email=email_do_autor))
